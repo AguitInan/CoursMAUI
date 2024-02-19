5ae9c866a720dcc6694dabac0bd47329e8d340b3
unknown <aguit.inan@gmail.com>
2024-02-19 16:43:23 +0100

    feat: update MainPage.xaml.cs

--- Exercice01/MainPage.xaml.cs
+++ Exercice01/MainPage.xaml.cs
@@ -19,5 +19,37 @@ namespace Exercice01
             mysteryNumber = random.Next(1, 101);
         }
 
+        private void OnCheckNumberClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(entryNumber.Text, out int userNumber))
+            {
+                if (userNumber == mysteryNumber)
+                {
+                    lblResult.Text = "Bravo ! Vous avez trouvé le nombre mystère.";
+                    lblResult.TextColor = Colors.Green;
+                    btnRestart.IsVisible = true;
+                }
+                else if (userNumber < mysteryNumber)
+                {
+                    lblResult.Text = "Le nombre mystère est plus grand.";
+                    lblResult.TextColor = Colors.Orange;
+                }
+                else
+                {
+                    lblResult.Text = "Le nombre mystère est plus petit.";
+                    lblResult.TextColor = Colors.Orange;
+                }
+                lblResult.IsVisible = true;
+            }
+            else
+            {
+                lblResult.Text = "Veuillez entrer un nombre valide.";
+                lblResult.TextColor = Colors.Red;
+                lblResult.IsVisible = true;
+            }
+
+            entryNumber.Text = string.Empty;
+        }
+
     }
 }
