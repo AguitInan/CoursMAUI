58c226179f02e14633a169ccedfff45d12fdb8ad
unknown <aguit.inan@gmail.com>
2024-02-19 16:43:01 +0100

    feat: update MainPage.xaml.cs

---
+++ Exercice01/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace Exercice01
+{
+    public partial class MainPage : ContentPage
+    {
+        private int mysteryNumber;
+        private Random random = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            mysteryNumber = random.Next(1, 101);
+        }
+
+    }
+}
