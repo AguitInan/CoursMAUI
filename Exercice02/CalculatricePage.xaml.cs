bed6f4506b80e51286cd55c64f74c22e0d4d6a66
unknown <aguit.inan@gmail.com>
2024-02-20 23:04:01 +0100

    feat: add CalculatricePage.xaml.cs

---
+++ Exercice02/CalculatricePage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace Exercice02;
+
+public partial class CalculatricePage : ContentPage
+{
+    public CalculatricePage()
+    {
+        InitializeComponent();
+    }
+}
