e823a203d8a7d9ce65d0ee40db0a376782c2bc54
unknown <aguit.inan@gmail.com>
2024-02-20 23:05:57 +0100

    feat: add CalculatriceStackLayoutsPage.xaml.cs

---
+++ Exercice02/CalculatriceStackLayoutsPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace Exercice02;
+
+public partial class CalculatriceStackLayoutsPage : ContentPage
+{
+    public CalculatriceStackLayoutsPage()
+    {
+        InitializeComponent();
+    }
+}
